120883a3c173c51a84828850caf2ae7ebeda1910
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2024-12-21 15:10:12 +0100

    add cudstom load

---
+++ readme.md
@@ -0,0 +1,1 @@
+Internalization package compatibile with npm package [@green-code-studio/internationalization](https://www.npmjs.com/package/@green-code-studio/internationalization)
--- src/TextsRepository.php
+++ src/TextsRepository.php
@@ -6,39 +6,33 @@ namespace Mkrawczyk\Internationalization;
 
 class TextsRepository
 {
-    static private ?I18nNode $root = null;
+    private ?I18nNode $root = null;
 
-    static public function getRootNode():I18nNode
+    public function getRootNode(): I18nNode
     {
         self::loadIfNeeded();
         return self::$root;
     }
 
-    static public function loadIfNeeded()
-    {
-        if (self::$root === null)
-            self::load();
-    }
 
-    static private function load()
+    public function loadModuleFile(string $module, string $filename)
     {
-        self::$root = new I18nNode();
-        $modules = scandir(__DIR__.'/../../');
-        foreach ($modules as $module) {
-            if ($module == '.' || $module == '..') {
-                continue;
-            }
-            $filename = __DIR__.'/../../'.$module.'/i18n.xml';
-            if (is_file($filename)) {
-                self::$root->addChild($module, self::parseFile($filename));
-            }
+        if ($this->root === null) {
+            $this->root = new I18nNode();
         }
+
+        $this->root->addChild($module, $this->parseFile($filename));
     }
 
-    private static function parseFile(string $filename)
+    public function loadRootFile(string $filename)
+    {
+        $this->root = $this->parseFile($filename);
+    }
+
+    private function parseFile(string $filename)
     {
         $xml = \simplexml_load_string(file_get_contents($filename));
-        if($xml===false)throw new \Exception("Bad i18n.xml file");
+        if ($xml === false) throw new \Exception("Bad i18n.xml file");
         return new I18nNode($xml);
     }
 }
